7eee1fa7603879b9c8d49da32a034d58413cdae0
margaretta hemmings <mbcc1905@hotmail.com>
2018-08-11 19:33:23 +0000

    automatically backed up by learn

---
+++ touch assignment.rb
@@ -0,0 +1,47 @@
+require 'pry'
+my_name = "Margaretta"
+
+my_name = Margaretta
+
+time = "afternoon"
+
+mood = "tired"
+
+def play_piano(mood, time, articles=5)
+  if mood =="relaxed"
+    puts "I'm Mozart"
+      elsif mood ==="tired"
+          puts "I'm only playing chopsticks"
+        end
+end
+
+def writing_paper(mood, time, article=5)
+  if time == "morning"
+    puts "I'll go to the library and do research"
+  elsif time == "evening"  
+puts "read the research  #{articles} that I have"
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+binding.pry
+puts "goodby"
